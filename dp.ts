7fcbd9439fb20bc1a88e1b46d153ea0b7dc035d8
suguru <sgr.inoue@gmail.com>
2020-12-25 16:59:00 +0100

    nl

--- dp.ts
+++ dp.ts
@@ -3,11 +3,8 @@ import config from "./config";
 
 export default () => {
   const dp = new DPBuilder(config);
-
   const parameters = dp.parameters();
-
   const input = dp.input();
-
   const result = dp.combine(parameters, input, ({ count }, text) =>
     Array.from({ length: count })
       .map(() => text)
