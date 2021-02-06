d0a5c0990164bfc5b466645d1228b4a65e371c25
suguru <sgr.inoue@gmail.com>
2021-02-06 11:11:38 +0100

    jest-preset

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,5 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist"],
+  setupFilesAfterEnv: ["@dpdpdp/jest-preset"],
 };
--- src/dp.test.ts
+++ src/dp.test.ts
@@ -1,53 +1,56 @@
 import dp from "./dp";
+import { Bot, Workflow } from "@dpdpdp/jest-preset";
 
-describe("dp", () => {
-  it("app/schema-requested", async () => {
-    expect(
-      await dp().receive({
-        type: "app/schema-requested",
-        replyToken:
-          "eyJhbGciOiJIUzI1NiJ9.MDAwMDAwMDAtMDAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMA.S72aHBcpsVuzyZmE2UWdHs8jNI2Aqdqj7th7b80hAzI",
-        parameters: { count: 3 },
-      })
-    ).toEqual([
-      {
-        type: "app/schema-generated",
-        replyToken:
-          "eyJhbGciOiJIUzI1NiJ9.MDAwMDAwMDAtMDAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMA.S72aHBcpsVuzyZmE2UWdHs8jNI2Aqdqj7th7b80hAzI",
-        inputSchema: {
-          type: "string",
-        },
-        inputUISchema: {
-          "ui:options": { rows: 3 },
-          "ui:widget": "https://widgets.dpdpdp.com/text@0.1.0",
-        },
-        outputSchema: {
-          type: "string",
-        },
-        outputUISchema: {
-          "ui:widget": "https://widgets.dpdpdp.com/text@0.1.0",
+describe("dpdpdp-template", () => {
+  it("has parameter schema", async () => {
+    const workflow = new Workflow(dp);
+
+    expect(workflow).toHaveParameterSchema({
+      properties: {
+        count: {
+          default: 1,
+          description: "Number of times to repeat given text",
+          maximum: 100,
+          minimum: 1,
+          title: "Count",
+          type: "integer",
         },
       },
-    ]);
+      required: ["count"],
+      type: "object",
+    });
+    expect(workflow).toHaveParameterUISchema({
+      count: {
+        "ui:widget": "https://widgets.dpdpdp.com/number@0.1.0",
+      },
+    });
   });
 
-  it("job/created", async () => {
-    expect(
-      await dp().receive({
-        type: "job/created",
-        replyToken:
-          "eyJhbGciOiJIUzI1NiJ9.MDAwMDAwMDAtMDAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMA.S72aHBcpsVuzyZmE2UWdHs8jNI2Aqdqj7th7b80hAzI",
-        input: "hello",
-        parameters: { count: 3 },
-      })
-    ).toEqual([
-      {
-        type: "job/completed",
-        replyToken:
-          "eyJhbGciOiJIUzI1NiJ9.MDAwMDAwMDAtMDAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMA.S72aHBcpsVuzyZmE2UWdHs8jNI2Aqdqj7th7b80hAzI",
-        output: "hello hello hello",
-        deps: [{ type: "input" }],
-      },
-    ]);
+  it("generates input and output schema", async () => {
+    const app = new Workflow(dp).createApp({ count: 3 });
+
+    expect(app).toHaveInputSchema({ type: "string" });
+    expect(app).toHaveInputUISchema({
+      "ui:options": { rows: 3 },
+      "ui:widget": "https://widgets.dpdpdp.com/text@0.1.0",
+    });
+    expect(app).toHaveOutputSchema({ type: "string" });
+    expect(app).toHaveOutputUISchema({
+      "ui:widget": "https://widgets.dpdpdp.com/text@0.1.0",
+    });
+  });
+
+  it("generates output", async () => {
+    const app = new Workflow(dp).createApp({ count: 1 });
+
+    expect(await app.processJob("你好", new Bot())).toOutput("你好");
+  });
+
+  it("repeats input text for the number of times specified in parameters", async () => {
+    const app = new Workflow(dp).createApp({ count: 3 });
+
+    expect(await app.processJob("hello", new Bot())).toOutput(
+      "hello hello hello"
+    );
   });
 });
